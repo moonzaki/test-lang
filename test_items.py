9f3658bc2dc2269daad2861f162c44579db421e6
moonzaki <71381734+moonzaki@users.noreply.github.com>
2020-09-17 01:02:57 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -2,5 +2,5 @@ def test_product_button(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
     import time
     time.sleep(5)
-    btest = browser.find_elements_by_css_selector('button.btn:nth-child(3)')
-    assert btest != 0, "Button not found"
+    btest = browser.find_elements_by_css_selector('.btn-add-to-basket')
+    assert btest, "Button not found"
